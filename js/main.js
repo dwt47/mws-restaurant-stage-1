128407fb779a170419797d663a254685b66a4abc
Daniel Thompson <dwt02011@mymail.pomona.edu>
2018-09-08 13:52:09 -0400

    fix: Added alt text to restaurant images
    Added text descriptions for each image to restaurants.json and added content to the HTML-rendering functions to pass along the alt text, using the restaurant's name as a fallback if no alt is provided.

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.altText || restaurant.name;
   li.append(image);
 
   const details = document.createElement('div');
