764a8b6ad84f97e624e898c8a7c906de7f146428
Daniel Thompson <dwt02011@mymail.pomona.edu>
2018-09-10 19:30:03 -0400

    feat: Update DBHelper to fetch data from server instead of .json file
    Updated DBHelper to fetch from localhost:1337 server instead of using the copied-in .json file

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,18 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(DBHelper.DATABASE_URL)
+      .then(res => res.json())
+      .then(restaurants => callback(null, restaurants))
+      .catch(error => callback(error, null));
   }
 
   /**
@@ -149,8 +140,10 @@ class DBHelper {
   /**
    * Restaurant image URL.
    */
-  static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+  static imageUrlForRestaurant({photograph, id}) {
+    // fall back to using 'id' if no photograph provided
+    // Kind of a sloppy solution but the API should really have all of the photographs
+    return (`/img/${photograph || id}.jpg`);
   }
 
   /**
